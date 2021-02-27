a0266e59a5397c69a5c1b70de095ebc5120eae18
marvthedonut <marvthedonut@hotmail.com>
2021-02-27 00:47:38 +0000

    t

--- commands/bal.js
+++ commands/bal.js
@@ -25,24 +25,36 @@ module.exports = {
       var userBal = await balance.findById(message.author.id)
       var inv = multipliers.map(item => multipliers.findKey(it => it == item)).join("\n")
       if(inv.length == 0) inv = "No items found."
-      bal.setDescription(`**Balance**: ${userBal.bal.toLocaleString()} test tubes\n\n**Inventory**:\n${inv}`)
-      .setAuthor(message.author.tag, message.author.displayAvatarURL({  dynamic:true  }))
+      // bal.setDescription(`**Balance**: ${userBal.bal.toLocaleString()} test tubes\n\n**Inventory**:\n${inv}`)
+      const Acknowledgments = ["ChemBot User"]
+      if(config.Acknowledgments[message.author.id]) {
+        config.Acknowledgments[message.author.id].forEach(it => Acknowledgments.push(it))
+      }
+      bal.addFields(
+        { value: `**Balance**: ${userBal.bal.toLocaleString()}\n\n**Inventory**:\n${inv}`, name: "\u200b", inline: true },
+        { value: `**Acknowledgments**:\n${Acknowledgments.join(", ")}`, name: "\u200b", inline: true })
+        .setAuthor(message.author.tag, message.author.displayAvatarURL({  dynamic:true  }))
     } else {
       var user = message.mentions.users.first() || await client.users.fetch(args[1])
-      var balances = mongoose.model("balance", Schemas.balances)
-      var userBal = await balances.findById(user.id)
-      if (!userBal) return message.reply("This person isn't participating in this!")
       var invs = mongoose.model("inventorie", Schemas.inventory)
       var inv = await invs.findById(user.id)
-      if(!inv) inv = await new invs({ _id: user.id }).save()
       inv.inv.forEach(item => {
         multipliers.set(item, config.shop.find(shopIt => shopIt.name == item).multiplier)
       })
       multipliers.sort((a, b) => b - a)
-      inv = multipliers.map(item => multipliers.findKey(it => it == item)).join("\n")
-      if(!inv) inv = "No items found."
-      bal.setDescription(`**Balance**: ${userBal.bal.toLocaleString()} test tubes\n\n**Inventory**:\n${inv}`)
-      .setAuthor(user.tag, user.displayAvatarURL({  dynamic:true  }))
+      var balance = mongoose.model("balance", Schemas.balances)
+      var userBal = await balance.findById(user.id)
+      var inv = multipliers.map(item => multipliers.findKey(it => it == item)).join("\n")
+      if(inv.length == 0) inv = "No items found."
+      // bal.setDescription(`**Balance**: ${userBal.bal.toLocaleString()} test tubes\n\n**Inventory**:\n${inv}`)
+      const Acknowledgments = ["ChemBot User"]
+      if(config.Acknowledgments[user.id]) {
+        config.Acknowledgments[user.id].forEach(it => Acknowledgments.push(it))
+      }
+      bal.addFields(
+        { value: `**Balance**: ${userBal.bal.toLocaleString()}\n\n**Inventory**:\n${inv}`, name: "\u200b", inline: true },
+        { value: `**Acknowledgments**:\n${Acknowledgments.join(", ")}`, name: "\u200b", inline: true })
+        .setAuthor(user.tag, user.displayAvatarURL({  dynamic:true  }))
     }
     message.channel.send(bal)
   }
